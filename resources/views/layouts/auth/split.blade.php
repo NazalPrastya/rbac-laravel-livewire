90bb71a3c1e574cc583be6523277ced19646bbd6
Nazal Prastya <nazalprastya@gmail.com>
2026-08-24 14:00:14 +0700

    chore: update auth page

--- resources/views/layouts/auth/split.blade.php
+++ resources/views/layouts/auth/split.blade.php
@@ -6,21 +6,21 @@
     <body class="min-h-screen bg-white antialiased dark:bg-linear-to-b dark:from-neutral-950 dark:to-neutral-900">
         <div class="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
             <div class="relative hidden h-full flex-col p-10 text-white lg:flex dark:border-e dark:border-neutral-800">
-    <div class="absolute inset-0 bg-neutral-900">
-        <img
-            src="/assets/background/kab-lahat.jpeg"
-            alt="kabupaten lahat"
-            class="h-full w-full object-cover"
-        />
-    </div>
+                <div class="absolute inset-0 bg-neutral-900">
+                    <img
+                        src="/assets/background/city.jpeg"
+                        alt="kabupaten lahat"
+                        class="h-full w-full object-cover"
+                    />
+                </div>
 
-    <a href="{{ route('home') }}" class="relative z-20 flex items-center text-lg font-medium" wire:navigate>
-        <span class="flex h-10 w-10 items-center justify-center rounded-md">
-            <x-app-logo-icon class="w-28" />
-        </span>
-        {{ config('app.name', 'Laravel') }}
-    </a>
-</div>
+                <a href="{{ route('home') }}" class="relative z-20 flex items-center text-lg font-bold text-black" wire:navigate>
+                    <span class="flex h-10 w-10 items-center justify-center rounded-md">
+                        <x-app-logo-icon class="w-28" />
+                    </span>
+                    SIPETARUNG
+                </a>
+            </div>
             <div class="w-full lg:p-8">
                 <div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
                     <a href="{{ route('home') }}" class="z-20 flex flex-col items-center gap-2 font-medium lg:hidden" wire:navigate>
